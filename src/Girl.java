4c9aeeff35e6b376ecd689cd1eb86001b9e7bd2c
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-07-11 12:30:53 -0400

    Completed all the practice exercises from the Inheritance and Type Casting GLAB

---
+++ src/Girl.java
@@ -0,0 +1,11 @@
+public class Girl extends Person{
+
+    static double ageFactor = 1.3;
+    public Girl(String aName){
+        name = "Ms. " + aName;
+    }
+
+//    public String talk(){
+//        return ("Hello! " + jump());
+//    }
+}
